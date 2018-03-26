e116a19a667f80100ce66f83bdacb6dc91299f07
Kevin <kperrine@gmail.com>
2018-03-26 12:23:35 -0500

    added errors in docs

--- src/immutable.ts
+++ src/immutable.ts
@@ -8,6 +8,17 @@ const arrayData = [
   ['abc123', { id: 'abc123', name: 'abc123' }]
 ];
 
+/**
+ * src/immutable.ts(11,42): error TS2345: Argument of type '(string | { id: string; name: string; })[][]' is not assignable to parameter of type 'Iterable<[string, SimpleObject]>'.
+  Types of property '[Symbol.iterator]' are incompatible.
+    Type '() => IterableIterator<(string | { id: string; name: string; })[]>' is not assignable to type '() => Iterator<[string, SimpleObject]>'.
+      Type 'IterableIterator<(string | { id: string; name: string; })[]>' is not assignable to type 'Iterator<[string, SimpleObject]>'.
+        Types of property 'next' are incompatible.
+          Type '(value?: any) => IteratorResult<(string | { id: string; name: string; })[]>' is not assignable to type '(value?: any) => IteratorResult<[string, SimpleObject]>'.
+            Type 'IteratorResult<(string | { id: string; name: string; })[]>' is not assignable to type 'IteratorResult<[string, SimpleObject]>'.
+              Type '(string | { id: string; name: string; })[]' is not assignable to type '[string, SimpleObject]'.
+                Property '0' is missing in type '(string | { id: string; name: string; })[]'.
+ */
 const mapOne = Map<string, SimpleObject>(arrayData);
 
 const objectData = {
@@ -16,4 +27,8 @@ const objectData = {
   abc123: { id: 'abc123', name: 'abc123' }
 };
 
+/**
+ * Argument of type '{ ghi789: { id: string; name: string; }; def456: { id: string; name: string; }; abc123: { id: str...' is not assignable to parameter of type 'Iterable<[string, SimpleObject]>'.
+  Property '[Symbol.iterator]' is missing in type '{ ghi789: { id: string; name: string; }; def456: { id: string; name: string; }; abc123: { id: str...'.
+ */
 const mapTwo = Map<string, SimpleObject>(objectData);
